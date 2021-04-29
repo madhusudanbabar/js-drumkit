eb3032b427fcb52ec1c4a4c3924c77a42d857904
madhusudan babar <madhusudanbabar@gmail.com>
2021-04-29 18:46:53 +0530

    completed project

---
+++ app.js
@@ -0,0 +1,32 @@
+window.addEventListener("keydown", (e) => {
+    // console.log(e.keyCode);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`) || null;
+    // key.forEach(child => {
+    //     if(child == "audio")
+    //     console.log({ "audio": child});
+    //     else
+    //     console.log({"garbage": child});
+    // });
+    // console.log(key);
+    const audio = key?.children[2] || 0;
+    if( !audio ) return;
+    // console.log({"audio": audio});
+
+    /* setInterval(() => {
+        key.classList.remove("playing");
+    }, 1000);
+    */
+
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add("playing");
+    key.addEventListener("transitionend", (e) => {
+        if(e.propertyName !== "transform") return
+        key.classList.remove("playing")
+    })
+
+    
+
+    // console.log(audio);
+});
